f93832bdc3347228ee1a0d7439fc645e69d27a8f
Ivy Tiongco <ivydoreen@gmail.com>
2020-05-28 10:13:56 -0700

    update app

--- assets/js/app.js
+++ assets/js/app.js
@@ -75,8 +75,8 @@ function updateToolTip(chosenXAxis, circlesGroup) {
   }
 
   var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    .offset([80, -60])
+    .attr("class", "d3-tip")
+    .offset([80, -60])  
     .html(function(d) {
       return (`${d.state}<br>${label}<br>${label2} ${d[chosenXAxis]}`);
     });
