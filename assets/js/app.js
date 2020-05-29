8abed0e27bf0ca0ee5f0244e17213806252a6f42
Ivy Tiongco <ivydoreen@gmail.com>
2020-05-29 13:49:06 -0700

    update app

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,12 +85,14 @@ function updateToolTip(chosenXAxis, circlesGroup) {
 
   circlesGroup.on("mouseover", function(data) {
     toolTip.show(data);
-    circlesGroup.style("stroke", "black");  //**** HOW TO MAKE ONLY SELECTED DATA PT HAVE BORDER, NOT ALL????????
+    d3.select(this)
+      .style("stroke", "black");
   })
     // onmouseout event
     .on("mouseout", function(data, index) {
       toolTip.hide(data);
-      circlesGroup.style("stroke", "transparent");
+      d3.select(this)
+        .style("stroke", "transparent");
     });
 
   return circlesGroup;
